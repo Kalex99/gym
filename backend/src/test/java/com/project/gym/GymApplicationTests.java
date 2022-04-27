6f445177960bf7ffbaf189480bbb9f0d5f82d9b3
Kovácsfi Alex <kovacsfialex@gmail.com>
2022-04-27 23:21:38 +0200

    Spring Boot jUnit

--- backend/src/test/java/com/project/gym/GymApplicationTests.java
+++ backend/src/test/java/com/project/gym/GymApplicationTests.java
@@ -1,13 +1,56 @@
 package com.project.gym;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GymApplicationTests {
+@AutoConfigureMockMvc
+public class GymApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
 
 	@Test
-	void contextLoads() {
+	public void findTermekByID() throws Exception {
+		mockMvc.perform(get("/termek/termekOlvasByID/2"))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+	@Test
+	public void findTermekByIDNotFound() throws Exception {
+		mockMvc.perform(get("/termek/termekOlvasByID/"))
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void findSzemelyiEdzoAll() throws Exception {
+		mockMvc.perform(get("/szemelyiEdzo/szemEdzoOlvas"))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void deleteFelhasznalo() throws Exception {
+		mockMvc.perform(delete("/felhasznalo/felhTorles/2"))
+				.andDo(print())
+				.andExpect(status().isOk());
 	}
 
 }
